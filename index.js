feb39570bb371aabff71ddbacb57c97932f6df77
Croluy <croluy@fbi.ac>
2023-08-03 17:55:39 +0200

    created npm project

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/**
+ * HTB_H4cksterbot - index.js - main file with all the commands.
+ * Copyright (C) 2023   Croluy
+ * 
+ * This program comes with ABSOLUTELY NO WARRANTY;
+ * This is free software, and you are welcome to redistribute it under certain conditions;
+ * See https://www.gnu.org/licenses/ for details.
+ */
+
