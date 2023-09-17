6bc13aaea0619878adf6d14e8acc9be40cefaba1
Croluy <croluy@fbi.ac>
2023-09-17 21:58:42 +0300

    updated bot with more commands

--- functions.js
+++ functions.js
@@ -1,19 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
+const editJsonFile = require("edit-json-file");
+let tokens_file = editJsonFile('./tokens.json', {autosave: true});
+let tokensListIndex=tokens_file.get("TokensNumber");
 
 //importing Replies from BotReplies.json
 const res = fs.readFileSync(path.resolve(__dirname, "Replies.json"));
 const Replies = JSON.parse(res);
-const {
-    functions: {
-        checkExpiredToken: {
-            log,
-            toAdmin,
-            toAdmin_1week
-        }
-    }
-} = Replies;
 
 /**
  * Parse template literal so it can be used in JSON elements.
@@ -32,31 +26,281 @@ function s(expression, variablesObj){
 }
 
 /**
- * If the token is expired, send a message to admin to update it and stops the bot.
- * If the token is about to expire in 1 week, suggests to admin to update it.
+ * Checks if a string is only made by numbers separated by /.
+ * The format is DD/MM/YYYY
  * 
- * @param {obj} expirationDateObj - object with the expiration date of the token (year, month, day)
+ * @param {string} input 
+ * @returns true if it's date, false otherwise
+ */
+function isOnlyNumbers(input) {
+    return /^\d{2}\/\d{2}\/\d{4}$/.test(input);
+}
+
+/**
+ * Initializes tokens.json file.
+ */
+function initFiles(){
+    // Reload file from the disk
+    tokens_file = editJsonFile(`./tokens.json`, {autosave: true});
+    //if the file is empty (tokens number doesn't exist), set token number to 0
+    if(tokens_file.get("TokensNumber") == null || tokens_file.get("TokensNumber") == undefined){
+        tokensListIndex=0;
+        tokens_file.set("TokensNumber",tokensListIndex);  //write the number of tokens at the beginning of the file
+        tokens_file.set("List",[]);  //clear the list of tokens
+    }
+    tokens_file.set("TokensNumber",tokensListIndex);  //write the number of tokens at the beginning of the file
+    tokens_file.save();
+    //if there is no token
+    if(tokens_file.get("TokensNumber") == 0){
+        //write creator as 1st token
+        tokens_file.append("List", {"ID":parseInt(process.env.CREATOR_ID), "Token":process.env.HTB_TOKEN,
+                                    "ExpirationDate":{"day": 1,"month": 7,"year": 2024}});
+        tokensListIndex++;   //increment number of the next token
+        tokens_file.set("TokensNumber",tokensListIndex);  //update token index
+        tokens_file.save();      //save file
+    }
+}
+
+/**
+ * Adds a new token to tokens.json file.
+ * If token is already saved, it replies to user with a message.
+ * 
+ * @param {obj} ctx - telegraf context
+ * @param {string} token - token to add
+ * @param {obj} d - date of expiration of token
+ */
+function add_TokenToFile(ctx,token,d){
+    //check if token is already in tokens.json file
+    if(isTokenInFile(ctx.from.id)) return ctx.reply(Replies.functions.__addTokenToFile.duplicate,{parse_mode: 'HTML'});
+
+    // Reload file from the disk
+    tokens_file = editJsonFile(`./tokens.json`, {autosave: true});
+
+    //if file is empty, fills it with the correct JSON structure
+    if(tokens_file.get("TokensNumber") == null || tokens_file.get("TokensNumber") == undefined){
+        tokensListIndex=0;
+        tokens_file.set("TokensNumber",tokensListIndex);  //write the number of tokens at the beginning of the file
+        tokens_file.set("List",[]);  //clear the list of tokens
+    }
+
+    tokens_file.append("List", {"ID":ctx.from.id, "Token":token,
+                                "ExpirationDate":{"day":d.day, "month":d.month, "year":d.year}});
+    tokensListIndex++;   //increment number of the next token
+    tokens_file.set("TokensNumber",tokensListIndex);  //update token index
+    tokens_file.save();
+    ctx.reply(Replies.functions.__addTokenToFile.added);
+}
+
+/**
+ * Check if token that we are trying to add is already in tokens.json file.
+ * 
+ * @param {int} id - id of the user
+ * @returns true if token is already in tokens.json file, false otherwise
+ */
+function isTokenInFile(id){
+    // Reload file from the disk
+    tokens_file = editJsonFile(`./tokens.json`, {autosave: true});
+    //check if token is already in tokens.json file
+    const tokensList = tokens_file.get("List");
+    for(let i=0; i<tokensListIndex; i++){
+        if(tokensList[i].ID==id){
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Get token position in tokens.json file.
+ * 
+ * @param {int} id - id of the user
+ * @returns position of token in tokens.json file, -1 if not found
+ */
+function getTokenPosition(id){
+    // Reload file from the disk
+    tokens_file = editJsonFile(`./tokens.json`, {autosave: true});
+    //check if token is already in tokens.json file
+    const tokensList = tokens_file.get("List");
+    for(let i=0; i<tokensListIndex; i++){
+        if(tokensList[i].ID==id){
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ * Get token from tokens.json file using user ID.
+ * 
+ * @param {int} id - id of the user
+ * @returns token from tokens.json file, -1 if not found
+ */
+function getToken(id){
+    // Reload file from the disk
+    tokens_file = editJsonFile(`./tokens.json`, {autosave: true});
+    //check if token is already in tokens.json file
+    const tokensList = tokens_file.get("List");
+    for(let i=0; i<tokensListIndex; i++){
+        if(tokensList[i].ID==id){
+            return tokensList[i].Token;
+        }
+    }
+    return -1;
+}
+
+/**
+ * Get expiration date of token in tokens.json file.
+ * 
+ * @param {int} id - id of the user
+ * @returns expiration date of token in tokens.json file, -1 if not found
+ */
+function getTokenExpirationDate(id){
+    // Reload file from the disk
+    tokens_file = editJsonFile(`./tokens.json`, {autosave: true});
+    //check if token is already in tokens.json file
+    const tokensList = tokens_file.get("List");
+    for(let i=0; i<tokensListIndex; i++){
+        if(tokensList[i].ID==id){
+            return tokensList[i].ExpirationDate;
+        }
+    }
+    return -1;
+}
+
+/**
+ * If the token is expired, send a message to user to update it.
+ * If the token is about to expire in 1 week, suggests user to update it.
+ * 
  * @param {obj} ctx - Telegram context
  */
-function checkExpiredToken(expirationDateObj,ctx){
+function checkExpiredToken(ctx){
+    const expirationDateObj = getTokenExpirationDate(ctx.from.id);
+    if(expirationDateObj==-1) return;   //Token not found
+
     const expirationDate = new Date(expirationDateObj.year,expirationDateObj.month,expirationDateObj.day);
     const currentDate = new Date();
     if(expirationDate-currentDate <= 0){    //Token expired
-        console.log(Replies.functions.checkExpiredToken.log);   //Prints to log to update the token
-        //send message to admin to update the token
-        ctx.telegram.sendMessage(process.env.CREATOR_ID,Replies.functions.checkExpiredToken.toAdmin,{parse_mode: 'HTML'});
-        process.exit(1);    //Stop the bot
+        //send message asking to update the token
+        ctx.reply(Replies.functions.checkExpiredToken.toAdmin,{parse_mode: 'HTML'});
     }else if(expirationDate-currentDate <= (1000 * 60 * 60 * 24 * 7)){
         //If expirationDate - currentDate <= 604800000 (7 days in milliseconds) the token is going to expire in 1 week
-        //send message to admin to update the token
-        ctx.telegram.sendMessage(process.env.CREATOR_ID,Replies.functions.checkExpiredToken.toAdmin_1week,{parse_mode: 'HTML'});
+        //send message asking to update the token soon
+        ctx.reply(Replies.functions.checkExpiredToken.toAdmin_1week,{parse_mode: 'HTML'});
     }
+}
+
+/**
+ * Using ctx object check if user has:
+ * 1. no username, no last name
+ * 2. no username
+ * 3. no last name
+ * 4. has all the fields
+ * 
+ * @param {obj} ctx - Telegram context
+ * @returns 1 if no username, no last name
+ * @returns 2 if no username
+ * @returns 3 if no last name
+ * @returns 4 if has all the fields
+ */
+function checkUserFields(ctx){
+    if(ctx.from.username==undefined && ctx.from.last_name==undefined){
+        return 1;
+    }else if(ctx.from.username==undefined){
+        return 2;
+    }else if(ctx.from.last_name==undefined){
+        return 3;
+    }
+    return 4;
+}
+
+/**
+ * Using ctx object starts the message with the name of the user using all possible fields.
+ * 
+ * @param {obj} ctx - Telegram context
+ * @returns string with the full name, username and id of the user
+ */
+function startReplyWith(ctx){
+    switch(checkUserFields(ctx)){
+        case 1:
+            return ctx.from.first_name+" [<code>"+ctx.from.id+"</code>]\n------------------\n";
+        case 2:
+            return ctx.from.first_name+" "+ctx.from.last_name+" [<code>"+ctx.from.id+"</code>]\n------------------\n";
+        case 3:
+            return ctx.from.first_name+" @"+ctx.from.username+" [<code>"+ctx.from.id+"</code>]\n------------------\n";
+        case 4:
+            return ctx.from.first_name+" "+ctx.from.last_name+" @"+ctx.from.username+" [<code>"+ctx.from.id+"</code>]\n------------------\n";
+    }
+}
+
+/**
+ * Deletes user message after (ms) milliseconds.
+ * 
+ * @param {obj} ctx Telegraf context
+ * @param {int} ms Time in milliseconds
+ */
+function deleteMessage(ctx,ms){
+    setTimeout(() => {
+        ctx.deleteMessage(ctx.message.message_id)
+            .catch((error) => {
+                console.error('Errore durante la cancellazione del messaggio:', error);
+            });
+    }, ms);
+}
+
+/**
+ * Deletes bot message after (ms) milliseconds.
+ * 
+ * @param {obj} ctx Telegraf context
+ * @param {obj} reply message to delete
+ * @param {int} ms Time in milliseconds
+ */
+function deleteBotMessage(ctx,reply,ms){
+    setTimeout(async () => {
+        try {
+            await ctx.telegram.deleteMessage(ctx.chat.id, reply.message_id);
+        } catch (error) {
+            console.error(error);
+        }
+    }, ms);
+
+    /*setTimeout(async () => {
+        await ctx.telegram.deleteMessage(ctx.chat.id,reply.message_id)
+            .catch((error) => {
+                console.error('Errore durante la cancellazione del messaggio:', error);
+            });
+    }, ms);*/
+}
+
+/**
+ * Checks if the chat is a group/supergroup/private chat.
+ * If it's a group/supergroup, checks if it's the original one.
+ * 
+ * @param {obj} ctx Telegram context
+ * @returns true if the chat is a group/supergroup/private chat
+ * @returns false if the chat is a group/supergroup but not the original one
+ */
+function checkGroup(ctx){
+    if(ctx.chat.type=="group" || ctx.chat.type=="supergroup"){
+        if(ctx.chat.id==process.env.GROUP_ID || ctx.chat.id == process.env.TEST_GROUP_ID) return true;
+        else return false;
+    }
+    return true;
 }
 
 //Old method, requires 'fun.' prefix. But it provides with functions description.
 module.exports = {
+    initFiles,
+    add_TokenToFile,
     s,
-    checkExpiredToken
+    isOnlyNumbers,
+    getToken,
+    getTokenExpirationDate,
+    checkExpiredToken,
+    checkUserFields,
+    startReplyWith,
+    deleteMessage,
+    deleteBotMessage,
+    checkGroup
 }
 
 /*//New method, does not require prefix. But it does not provide with functions description.
--- index.js
+++ index.js
@@ -8,23 +8,30 @@
  * See https://www.gnu.org/licenses/ for details.
  */
 
-require("dotenv").config();
+const axios = require('axios');
 const {Telegraf} = require('telegraf');
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 
+//HTB Ranks
+const rankString = {
+    1: 'Noob',
+    2: 'Script Kiddie',
+    3: 'Hacker',
+    4: 'Pro Hacker',
+    5: 'Elite Hacker',
+    6: 'Guru',
+    7: 'Omniscient'
+};
+
+let botReply=null;  //variable to store the bot reply message, so it can be deleted after a certain amount of time
+const msDelUserMessage=0;   //time in ms after which the user message is deleted
+const msDelBotMessage=60000;    //time in ms after which the bot message is deleted
+
 //importing Replies from BotReplies.json
 const res = fs.readFileSync(path.resolve(__dirname, "Replies.json"));
 const Replies = JSON.parse(res);
-const {
-    index: {
-        __expireToken:{
-            daysonly,
-            daysandweeks,
-        }
-    }
-} = Replies;
 
 //Old method, requires 'fun.' prefix. But it provides with functions description.
 const fun = require('./functions.js');
@@ -33,21 +40,132 @@ const fun = require('./functions.js');
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
-//Bot token expires after 1 year, save the creation date (months and days start at 0, so 2024-07-01 is 2024-08-02 for the program)
-const HTBtokenExpiration={
-    year: 2024,
-    month: 8  -1,
-    day: 2  -1
-}
+bot.start(async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.initFiles();
+    fun.checkExpiredToken(ctx);
 
-bot.start((ctx) => {
-    fun.checkExpiredToken(HTBtokenExpiration,ctx);
-    ctx.reply('Welcome to HTB_H4cksterbot!');
+    botReply=await ctx.reply(Replies.index.__start.start,{parse_mode: 'HTML'});
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
 });
 
-bot.command('expire', (ctx) => {
-    fun.checkExpiredToken(HTBtokenExpiration,ctx);
-    if(ctx.message.from.id!=process.env.CREATOR_ID) return; //Only creator can use this command
+bot.help(async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.checkExpiredToken(ctx);
+
+    //send the help message
+    botReply=await ctx.reply(Replies.index.__help.help,{parse_mode: 'HTML'});
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.command('token', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    //token can only be set in private chat, as the token is a sensitive information and anyone with access to it is able to retrieve loads of data about your account
+    if(ctx.message.chat.type!='private'){
+        //delete message sent in group
+        await ctx.deleteMessage(ctx.message.message_id);
+        botReply=await ctx.reply(Replies.index.__token.not_private,{parse_mode: 'HTML'});
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+
+    //lets the user pass the token as an argument and adds it to tokens.json
+    const args = ctx.message.text.split(' ');
+    if(args.length!=3){
+        botReply=await ctx.reply(Replies.index.__token.wrong_args,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+    const d = args[1];
+    const token = args[2];
+
+    //check if args are valid, date must be numbers and / and must be 10 digit, token must be 995 characters long
+    if(token.length!=995 || d.length!=10 || !fun.isOnlyNumbers(d)){
+        botReply=await ctx.reply(Replies.index.__token.wrong_args,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+    const date = {
+        "day": parseInt(d.substring(0,2), 10)-1,
+        "month": parseInt(d.substring(3,5), 10)-1,
+        "year": parseInt(d.substring(6), 10)
+    };
+    //check if expiration date provided has already passed
+    const currentDate = new Date();
+    const expirationDate = new Date(date.year,date.month,date.day);
+    if(expirationDate-currentDate <= 0){    //Token expired
+        botReply=await ctx.reply(Replies.index.__token.wrong_args,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+
+    //check if token is valid
+    axios.get('https://www.hackthebox.com/api/v4/user/info', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(async (response) => {
+        //if token is valid, add it to tokens.json
+        botReply=await fun.add_TokenToFile(ctx,token,date);
+    }).catch(async (err) => {
+        //if token is not valid, send a message to the user
+        botReply=await ctx.reply(fun.s(Replies.index.__token.invalid_token,{tok:token}),{parse_mode: 'HTML'});
+    });
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.command('instructions', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.checkExpiredToken(ctx);
+    botReply=await ctx.reply(Replies.index.__instructions.how_to_set,{parse_mode: 'HTML'});
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.command('expire', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.checkExpiredToken(ctx);
+
+    const token=fun.getToken(ctx.from.id);
+    if(token==-1){
+        botReply=await ctx.reply(Replies.index.no_token_found,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+
+    //get token expiration date and if it's not found, send a message to the user
+    const HTBtokenExpiration = fun.getTokenExpirationDate(ctx.from.id);
+    if(HTBtokenExpiration==-1){
+        botReply=await ctx.reply(Replies.index.__expireToken.no_token,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
 
     //if day or month is <10, add a 0 in front so it's a 2 digit number
     let d=HTBtokenExpiration.day+1;
@@ -57,16 +175,129 @@ bot.command('expire', (ctx) => {
 
     //get number of days and weeks left for token expiration
     const currentDate = new Date();
-    const expirationDate = new Date(HTBtokenExpiration.year,HTBtokenExpiration.month,HTBtokenExpiration.day);
+    const expirationDate = new Date(HTBtokenExpiration.year,HTBtokenExpiration.month,HTBtokenExpiration.day+1);
     let daysLeft = Math.floor((expirationDate-currentDate) / (1000 * 60 * 60 * 24));
     const weeksLeft = Math.floor((expirationDate-currentDate) / (1000 * 60 * 60 * 24 * 7));
 
     if(weeksLeft>1){
         daysLeft=daysLeft-weeksLeft*7;
-        ctx.reply(fun.s(Replies.index.__expireToken.daysandweeks,{year:HTBtokenExpiration.year,month:m,day:d,daysLeft:daysLeft,weeksLeft:weeksLeft}),{parse_mode: 'HTML'});
+        botReply=await ctx.reply(fun.s(Replies.index.__expireToken.daysandweeks,{year:HTBtokenExpiration.year,month:m,day:d,daysLeft:daysLeft,weeksLeft:weeksLeft}),{parse_mode: 'HTML'});
     }else{
-        ctx.reply(fun.s(Replies.index.__expireToken.daysonly,{year:HTBtokenExpiration.year,month:m,day:d,daysLeft:daysLeft}),{parse_mode: 'HTML'});
+        botReply=await ctx.reply(fun.s(Replies.index.__expireToken.daysonly,{year:HTBtokenExpiration.year,month:m,day:d,daysLeft:daysLeft}),{parse_mode: 'HTML'});
     }
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.command('rank', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.checkExpiredToken(ctx);
+
+    //get token, if not found send a message to user
+    const token=fun.getToken(ctx.from.id);
+    if(token==-1){
+        botReply=await ctx.reply(Replies.index.no_token_found,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+
+    try{
+        const response = await axios.get('https://www.hackthebox.com/api/v4/user/info', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        let rank = rankString[response.data.info.rank_id];
+        //if(response.data.info.id == process.env.CREATOR_HTB_ID) rank="Omniscient";   //Troll with creator rank being Omniscient :P
+        botReply=await ctx.reply(fun.startReplyWith(ctx)+fun.s(Replies.index.__rank.mess_rank,{rank:rank}),{parse_mode: 'HTML'});
+    }catch(err){
+        botReply=await ctx.reply(fun.s(Replies.index.req_error,{creator:process.env.CREATOR_USERNAME, error:err, headers:"No Headers"}),{parse_mode: 'HTML'});
+    }
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.command('id', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.checkExpiredToken(ctx);
+
+    //get token, if not found send a message to user
+    const token=fun.getToken(ctx.from.id);
+    if(token==-1){
+        botReply=await ctx.reply(Replies.index.no_token_found,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+
+    try{
+        const response = await axios.get('https://www.hackthebox.com/api/v4/user/info', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        botReply=await ctx.reply(fun.startReplyWith(ctx)+fun.s(Replies.index.__id.mess_id,{id:response.data.info.id}),{parse_mode: 'HTML'});
+    }catch(err){
+        botReply=await ctx.reply(fun.s(Replies.index.req_error,{creator:process.env.CREATOR_USERNAME,error:err+"\n"+err.response.data.error,headers:err.config.headers}),{parse_mode: 'HTML'});
+    }
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.command('team', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    fun.checkExpiredToken(ctx);
+
+    //get token, if not found send a message to user
+    const token=fun.getToken(ctx.from.id);
+    if(token==-1){
+        botReply=await ctx.reply(Replies.index.no_token_found,{parse_mode: 'HTML'});
+
+        //Delete messages
+        fun.deleteMessage(ctx,msDelUserMessage);
+        fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+        return;
+    }
+
+    try{
+        const response = await axios.get('https://www.hackthebox.com/api/v4/user/info', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        let team=response.data.info.team;
+        if(team==null) team="No Team";
+        botReply=await ctx.reply(fun.startReplyWith(ctx)+fun.s(Replies.index.__team.mess_team,{team:team}),{parse_mode: 'HTML'});
+    }catch(err){
+        botReply=await ctx.reply(fun.s(Replies.index.req_error,{creator:process.env.CREATOR_USERNAME, error:err, headers:"No Headers"}),{parse_mode: 'HTML'});
+    }
+
+    //Delete messages
+    fun.deleteMessage(ctx,msDelUserMessage);
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
+});
+
+bot.on('text', async (ctx) => {
+    if(fun.checkGroup(ctx)==false) return;
+    const message = ctx.message.text;
+
+    //message to test if bot is online
+    if(message.toLowerCase()==='ping'){
+        botReply=await ctx.reply('pong');
+        fun.deleteMessage(ctx,msDelUserMessage);
+    }
+    fun.deleteBotMessage(ctx,botReply,msDelBotMessage);
 });
 
 //Has the be at the end of the file
